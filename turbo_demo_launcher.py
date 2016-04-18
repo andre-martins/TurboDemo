824623ffcfeb6a36188484fec1b2cbd211d3fc4f
Andre Martins <andre.t.martins@gmail.com>
2016-04-18 13:51:29 -0400

    Added requirements file and did some small fixes.

--- turbo_demo_launcher.py
+++ turbo_demo_launcher.py
@@ -3,10 +3,12 @@
 import os
 import sys
 
-TURBO_PARSER_HOME = '/home/afm/projects/TurboParser'
+TURBO_PARSER_HOME = '/root/TurboParser'
 TURBO_PARSER_PYTHON = os.sep.join([TURBO_PARSER_HOME, 'python'])
 TURBO_PARSER_LIBS = os.sep.join([TURBO_PARSER_HOME,  'deps', 'local', 'lib'])
 
+if 'LD_LIBRARY_PATH' not in os.environ:
+    os.environ['LD_LIBRARY_PATH'] = ''
 os.environ['LD_LIBRARY_PATH'] += ':' + TURBO_PARSER_LIBS
 os.environ['LD_LIBRARY_PATH'] += ':' + TURBO_PARSER_PYTHON
 os.environ['PYTHONPATH'] = ':' + TURBO_PARSER_PYTHON
